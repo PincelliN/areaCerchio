7fb844f7214cd1e8cf0c4aca008fbca183046b19
Nicolò <pincelli.n@gmail.com>
2025-01-05 14:03:15 +0100

    add double area

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        Console.WriteLine("inserisci il raggio del cerchio che vuoi calcolare:");
+        string number=Console.ReadLine();
+        bool isDouble =double.TryParse(number,out double numberDouble);
+        if(isDouble){
+            double area = numberDouble*numberDouble*Math.PI;
+            Console.WriteLine(area); 
+        }
